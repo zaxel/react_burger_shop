d77af42b5aa588e2e7b3ff5ea73dd5e665925d28
zaxel <sae11.z.alex@gmail.com>
2021-08-29 12:54:22 +0300

    404 page

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import Menu from "./Menu";
 import Orders from "./Orders";
 import MenuAdmin from "./MenuAdmin";
+import NotFound from "./NotFound";
 
 class App extends React.Component{
     render(){
         return(
             <div className='burger'>
-                <Menu title='Hot Burgers Best'/>
+                {/* <Menu title='Hot Burgers Best'/>
                 <Orders/>
-                <MenuAdmin/>
+                <MenuAdmin/> */}
+                <NotFound/>
             </div>
         )
     }
