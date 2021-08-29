bf1fad40b01c539ab38bd2022e957ee01c1d5105
zaxel <sae11.z.alex@gmail.com>
2021-08-29 03:37:44 +0300

    Header component styles added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,13 @@ class Header extends React.Component{
     render(){
         return( 
             <div className='menu__header menu-header'>
-                Header Content
+                <div className='menu-header__rating header-rating'>
+                    <div className='header-rating__text'>Rating: </div>
+                    <div className='header-rating__stars'> *****</div>
+                </div>
+                <span className='menu-header__line'></span>
+                <h1 className='menu-header__slogan'>Hot Burgers</h1>
+                <h2 className='menu-header__descr'> Hot <span className='menu-header__highlight'>burgers</span> and quick delivery</h2>
             </div>
         )
     }
