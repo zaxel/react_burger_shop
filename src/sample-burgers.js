a3f0d19b81c80b7a63ab9769848c95b94935fa08
zaxel <sae11.z.alex@gmail.com>
2021-08-30 01:18:11 +0300

    burgers example object added

---
+++ src/sample-burgers.js
@@ -0,0 +1,61 @@
+//list of restaurants
+const sample_burgers = {
+    burger1: {
+        name: 'The Impossible Burger',
+        price: 160.50,
+        status: 'available',
+        descr: 'impossible patty, american cheese, pickles, onion, mustard, ketchup',
+        image: 'src/img/burger1.jpg',
+    },
+    burger2: {
+        name: 'The Original',
+        price: 180.00,
+        status: 'available',
+        descr: 'american cheese, pickles, onion, mustard, ketchup',
+        image: 'src/img/burger2.jpg',
+    },
+    burger3: {
+        name: 'The Cheesy Chili',
+        price: 150.50,
+        status: 'available',
+        descr: 'homemade chili, melted cheese, BBQ ranch, caramelized onions',
+        image: 'src/img/burger3.jpg',
+    },
+    burger4: {
+        name: 'The Classic',
+        price: 130.50,
+        status: 'available',
+        descr: 'american cheese, pickles, lettuce, tomato, caramelized onions, Burger Shop secret dressing',
+        image: 'src/img/burger4.jpg',
+    },
+    burger5: {
+        name: 'Wine Maker',
+        price: 220.50,
+        status: 'available',
+        descr: 'saint agur blue cheese cream, grilled grapes, white cheddar, and port wine glaze',
+        image: 'src/img/burger5.jpg',
+    },
+    burger6: {
+        name: 'The 3pm',
+        price: 200.00,
+        status: 'available',
+        descr: 'bacon, crispy onion ring, american cheese, and bbq sauce',
+        image: 'src/img/burger6.jpg',
+    },
+    burger7: {
+        name: 'Toro Bravo',
+        price: 140.50,
+        status: 'available',
+        descr: 'roasted hatch chilies, candied bacon, white cheddar, and habanero aioli',
+        image: 'src/img/burger7.jpg',
+    },
+    burger8: {
+        name: 'Loaded Grill Cheese',
+        price: 250.00,
+        status: 'available',
+        descr: 'caramelized onions, tomatoes, pickles, avocado, white cheddar and cheese sauce',
+        image: 'src/img/burger4.jpg',
+    },
+}
+
+export default sample_burgers;
