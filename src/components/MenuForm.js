7bd24e58347294c23f3dc69e389d4da801a3e349
zaxel <sae11.z.alex@gmail.com>
2021-08-29 22:49:21 +0300

    getting values from burger form fields with createRef and making burger object with it

--- src/components/MenuForm.js
+++ src/components/MenuForm.js
@@ -1,23 +1,39 @@
 import React from 'react';
 
-const formHandler = (e) => {
-    e.preventDefault();
-}
+
 
 class MenuForm extends React.Component{
+
+    titleRef = React.createRef();
+    priceRef = React.createRef();
+    statusRef = React.createRef();
+    descrRef = React.createRef();
+    imageRef = React.createRef();
+
+    createBurger = (e) => {
+        e.preventDefault();
+        const burger = {
+            name: this.titleRef.current.value,
+            price: parseFloat(this.priceRef.current.value),
+            status: this.statusRef.current.value,
+            descr: this.descrRef.current.value,
+            image: this.imageRef.current.value,
+        }
+        console.log(burger)
+    }
     render(){
         return(
-            <form onSubmit={formHandler} className='editor__form editor-form'>
+            <form onSubmit={this.createBurger} className='editor__form editor-form'>
                 <div className='editor-form__main'>
-                    <input type="text" name="burger_title" placeholder="Title"/>
-                    <input type="number" name="burger_price" placeholder="Price"/>
+                    <input ref={this.titleRef} type="text" name="burger_title" placeholder="Title"/>
+                    <input ref={this.priceRef} type="number" step=".01" name="burger_price" placeholder="Price"/>
                 </div>
-                <select name="burger_status">
-                    <option defaultValue="available">Available</option>
+                <select ref={this.statusRef} name="burger_status">
+                    <option defaultValue="status">available</option>
                     <option value="out_of_stock">Out of stock</option>
                 </select>
-                <textarea name="burger_descr" placeholder="Enter burger description" rows="3"/>
-                <input type="text" name="burger_img" placeholder="Image"/>
+                <textarea ref={this.descrRef} name="burger_descr" placeholder="Enter burger description" rows="3"/>
+                <input ref={this.imageRef} type="text" name="burger_img" placeholder="Image"/>
                 <button className="select-button">Add To Menu<span>Add To Menu</span></button>
             </form>
         )
