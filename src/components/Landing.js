f2c8436567fe46c9098ba6451f4bf3b68ec2d24f
zaxel <sae11.z.alex@gmail.com>
2021-08-29 13:59:37 +0300

    restaurants pages router

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -19,7 +19,8 @@ class Landing extends React.Component {
         })
     }
     visitRestaurant = () => {
-        console.log('visited')
+        const {url} = this.state;
+        this.props.history.push(`/restaurant/${url}`)
     }
 
     render() {
--- src/sample-restaurants.js
+++ src/sample-restaurants.js
@@ -2,17 +2,17 @@
 const restaurants = [
     {
         title: 'Hot Burger Opernyi Teatr',
-        url: 'horburgerteatr.lviv.ua',
+        url: 'horburgerteatr-lviv-ua',
         id: 1
     },
     {
         title: 'Hot Burger Varenichna',
-        url: 'varenyky.lviv.ua',
+        url: 'varenyky-lviv-ua',
         id: 2
     },
     {
         title: 'Hot Burger Cheburechna',
-        url: 'cheburator.lviv.ua',
+        url: 'chebureckh-lviv-ua',
         id: 3
     },
 ]
