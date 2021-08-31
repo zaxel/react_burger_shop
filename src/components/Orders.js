88f0d59d4130c9253c9b0ada90cb22968c89e14a
zaxel <sae11.z.alex@gmail.com>
2021-08-31 13:22:42 +0300

    order component ordered items names and quantity display

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,18 +1,24 @@
 import React from "react";
 
+
 class Orders extends React.Component{
-    key = Object.keys(this.props.burgers);
+
     render(){
+        const burgers = this.props.burgers;
+        const order = this.props.order;
+        const keys = Object.keys(order);
         return(
             <div className='burgers__orders orders'>
                 <h2 className='orders__tittle'>
                     Your Order
                 </h2>
-                <div className='orders__list-cont'>
+                <ul className='orders__list-cont'>
                     <h3 className='orders__list-noitem'>Please make your order</h3>
-                    <div className='orders__list-item'>3 x The Impossible Burger</div>
-                    <div className='orders__list-item'>1 x The Original</div>
-                </div>
+                    
+                    {keys.map((item)=>{
+                        return <li className='orders__list-item'>{order[item]} x {burgers[item].name}</li>
+                    })}
+                </ul>
                 <div className='orders__total-cont'>
                     <h3 className='orders__delivery'>Delivery: &#163; 120.00</h3>
                     <div className='orders__del-descr'>spend another &#163; 55.00 to get discounted delivery for only &#163; 10.00</div>
