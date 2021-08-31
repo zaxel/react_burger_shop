cefb5f7c8b3fbbe94adeb326b59bcc4073713b96
zaxel <sae11.z.alex@gmail.com>
2021-08-31 15:04:28 +0300

    order component total price minus item price that become out of stock

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -2,27 +2,39 @@ import React from "react";
 
 
 class Orders extends React.Component{
+    
 
     render(){
+        
         const burgers = this.props.burgers;
         const order = this.props.order;
         const keys = Object.keys(order);
+
+        
+
+        const total = keys.reduce((acc, item)=>{
+            if(burgers[item].status !== 'available')
+            return acc
+
+            return acc + burgers[item].price * order[item];
+        }, 0)
+        
         return(
             <div className='burgers__orders orders'>
                 <h2 className='orders__tittle'>
-                    Your Order
+                    Your Order 
                 </h2>
                 <ul className='orders__list-cont'>
                     <h3 className='orders__list-noitem'>Please make your order</h3>
                     
                     {keys.map((item)=>{
-                        return <li className='orders__list-item'>{order[item]} x {burgers[item].name}</li>
+                        return <li key={item} className='orders__list-item'>{order[item]} x {burgers[item].name}</li>
                     })}
                 </ul>
                 <div className='orders__total-cont'>
                     <h3 className='orders__delivery'>Delivery: &#163; 120.00</h3>
                     <div className='orders__del-descr'>spend another &#163; 55.00 to get discounted delivery for only &#163; 10.00</div>
-                    <h3 className='orders__total'>Total: &#163; 320.00</h3>
+                    <h3 className='orders__total'>Total: &#163; {total}</h3>
                 </div>
             </div>
             
