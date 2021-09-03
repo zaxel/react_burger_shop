87df49375fc18120964b16a66b1eef429b68d605
zaxel <sae11.z.alex@gmail.com>
2021-09-04 00:28:31 +0300

    set values to each field in every edit menu form from state

--- src/components/MenuEditForm.js
+++ src/components/MenuEditForm.js
@@ -8,15 +8,15 @@ class MenuEditorForm extends React.Component{
         return(
             <form className='editor__form editor-form'>
                 <div className='editor-form__main'>
-                    <input type="text" name="burger_title" placeholder="Title"/>
-                    <input type="number" step=".01" name="burger_price" placeholder="Price"/>
+                    <input type="text" name="burger_title" value={this.props.burger.name}/>
+                    <input type="number" step=".01" name="burger_price" value={this.props.burger.price}/>
                 </div>
-                <select name="burger_status">
-                    <option defaultValue="status">available</option>
+                <select name="burger_status" value={this.props.burger.status}>
+                    <option value="status">available</option>
                     <option value="out_of_stock">Out of stock</option>
                 </select>
-                <textarea name="burger_descr" placeholder="Enter burger description" rows="3"/>
-                <input type="text" name="burger_img" placeholder="Image"/>
+                <textarea name="burger_descr" rows="3" value={this.props.burger.descr}/>
+                <input type="text" name="burger_img" value={this.props.burger.image}/>
         
             </form>
         )
