57b012da2ff9e99bf8cbb39843bd1411d226f8b3
zaxel <sae11.z.alex@gmail.com>
2021-09-05 01:52:26 +0300

    got css transition work finaly..

--- src/components/MenuAdmin.js
+++ src/components/MenuAdmin.js
@@ -2,6 +2,7 @@ import React from "react";
 import MenuForm from "./MenuForm";
 import sample_burgers from "../sample-burgers";
 import MenuEditorForm from "./MenuEditForm";
+import { TransitionGroup } from "react-transition-group"
 
 class MenuAdmin extends React.Component{
     
@@ -10,10 +11,12 @@ class MenuAdmin extends React.Component{
             <div className='burgers__editor editor'>
                 <h2 className='editor__tittle'>Edit Menu</h2>
                 <div className='editor__forms'>
-                {Object.keys(this.props.burgers).map((key)=> {
-                    return <MenuEditorForm key={key} index={key} burger={this.props.burgers[key]} changeBurger={this.props.changeBurger} removeBurger={this.props.removeBurger}/>
-                })}
+                
+                        {Object.keys(this.props.burgers).map((key)=> {
+                            return <MenuEditorForm key={key} index={key} burger={this.props.burgers[key]} changeBurger={this.props.changeBurger} removeBurger={this.props.removeBurger}/>
+                        })}
                     <MenuForm {...this.props}/>
+                
                 </div>
                 <button onClick={() => this.props.loadSamples(sample_burgers)} className="editor__button select-button">Load Burgers Samples<span>Load Burgers Samples</span></button>
             </div>
--- src/components/MenuEditForm.js
+++ src/components/MenuEditForm.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 
 
 class MenuEditorForm extends React.Component{
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+          loaded: false
+        }
+        this.loadQuote = this.loadQuote.bind(this)
+      }
+      loadQuote() {
+        this.setState({ loaded: !this.state.loaded });
+      }
     
     changeHandler = (e) => {
         const formData = {
@@ -15,24 +24,30 @@ class MenuEditorForm extends React.Component{
     }
     
     render(){
-        
+        const { loaded } = this.state;
         return(
-            <form onSubmit={(e)=>{e.preventDefault(); this.props.removeBurger(this.props.index)}} className='editor__form editor-form'>
-                <div className='editor-form__main'>
-                    <input onChange={this.changeHandler} type="text" name="name" value={this.props.burger.name}/>
-                    <input onChange={this.changeHandler} type="number" step=".01" name="price" value={this.props.burger.price}/>
-                </div>
-                <select onChange={this.changeHandler} name="status" value={this.props.burger.status}>
-                    <option value="available">available</option>
-                    <option value="out_of_stock">Out of stock</option>
-                </select>
-                <textarea onChange={this.changeHandler} name="descr" rows="3" value={this.props.burger.descr}/>
-                <input onChange={this.changeHandler} type="text" name="image" value={this.props.burger.image}/>
-                <button className="select-button">remove item<span>remove item</span></button>
-            </form>
-        )
-        
+            <>
+                <button onClick={this.loadQuote}>click</button>
+                <CSSTransition in={loaded} timeout={{enter: 5000, exit: 5000}} classNames="edtr">
+                    <div>Test my test</div>                
+                </CSSTransition>
 
+                    <form onSubmit={(e)=>{e.preventDefault(); this.props.removeBurger(this.props.index)}} className='editor__form editor-form'>
+                        <div className='editor-form__main'>
+                            <input onChange={this.changeHandler} type="text" name="name" value={this.props.burger.name}/>
+                            <input onChange={this.changeHandler} type="number" step=".01" name="price" value={this.props.burger.price}/>
+                        </div>
+                        <select onChange={this.changeHandler} name="status" value={this.props.burger.status}>
+                            <option value="available">available</option>
+                            <option value="out_of_stock">Out of stock</option>
+                        </select>
+                        <textarea onChange={this.changeHandler} name="descr" rows="3" value={this.props.burger.descr}/>
+                        <input onChange={this.changeHandler} type="text" name="image" value={this.props.burger.image}/>
+                        <button className="select-button">remove item<span>remove item</span></button>
+                    </form>
+            </>
+            
+        )
     }
 }
 
