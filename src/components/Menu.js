0d039136981abbfe5dbc73bfa306521e91f47136
zaxel <sae11.z.alex@gmail.com>
2021-09-05 14:54:01 +0300

    transition group module sliding effect added to burger menu item

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,17 +1,24 @@
 import React from "react";
 import Header from "./Header";
 import Burger from "./Burger";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 class Menu extends React.Component{
     render(){
         return(
             <div className='burgers__menu menu'>
                 <Header {...this.props}/>
-                <ul className='burgers__list'>
+                {/* <ul className='burgers__list'> */}
+                <TransitionGroup component="ul" className="burgers__list">
                     {Object.keys(this.props.burgers).map((el)=>{
-                        return <li className='burger' key={el}><Burger burger={this.props.burgers[el]} index={el} addOrder={this.props.addOrder}/></li>;
+                        return(
+                            <CSSTransition classNames="burgers__list" key={el} timeout={{enter: 800, exit: 800}}>
+                             <li className='burger' key={el}><Burger burger={this.props.burgers[el]} index={el} addOrder={this.props.addOrder}/></li>
+                            </CSSTransition>
+                        )
                     })}
-                </ul>
+                </TransitionGroup>
+                {/* </ul> */}
             </div>
             
         )
