9a03b2143e974a20653f848d25118fab547cf5e9
zaxel <sae11.z.alex@gmail.com>
2021-09-05 03:26:36 +0300

    edit burger form flies in and out on add and remove burger

--- src/components/MenuAdmin.js
+++ src/components/MenuAdmin.js
@@ -2,7 +2,7 @@ import React from "react";
 import MenuForm from "./MenuForm";
 import sample_burgers from "../sample-burgers";
 import MenuEditorForm from "./MenuEditForm";
-import { TransitionGroup } from "react-transition-group"
+import { TransitionGroup, CSSTransition } from "react-transition-group"
 
 class MenuAdmin extends React.Component{
     
@@ -10,14 +10,18 @@ class MenuAdmin extends React.Component{
         return(
             <div className='burgers__editor editor'>
                 <h2 className='editor__tittle'>Edit Menu</h2>
-                <div className='editor__forms'>
-                
+                {/* <div className='editor__forms'> */}
+                    <TransitionGroup component='div' className="edtr">
                         {Object.keys(this.props.burgers).map((key)=> {
-                            return <MenuEditorForm key={key} index={key} burger={this.props.burgers[key]} changeBurger={this.props.changeBurger} removeBurger={this.props.removeBurger}/>
+                            return(
+                                <CSSTransition classNames='edtr' key={key} timeout={{enter: 800, exit: 800}}>
+                                    <MenuEditorForm key={key} index={key} burger={this.props.burgers[key]} changeBurger={this.props.changeBurger} removeBurger={this.props.removeBurger}/>
+                                </CSSTransition>)
                         })}
                     <MenuForm {...this.props}/>
+                    </TransitionGroup>
                 
-                </div>
+                {/* </div> */}
                 <button onClick={() => this.props.loadSamples(sample_burgers)} className="editor__button select-button">Load Burgers Samples<span>Load Burgers Samples</span></button>
             </div>
             
