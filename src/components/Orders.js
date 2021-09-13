ed516632e2e8b24aa1807aa85374af2417d7067c
zaxel <sae11.z.alex@gmail.com>
2021-09-13 12:52:40 +0300

    order component fixed on small screens

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -54,34 +54,36 @@ class Orders extends React.Component{
         }
         return(
             <div className='burgers__orders orders'>
-                <h2 className='orders__tittle'>
-                    Your Order 
-                </h2>
-                {!keys.length && <h3 className='orders__list-noitem'>Please make your order</h3>}
-                <TransitionGroup component='ul' className='orders__list-cont'>
-                    {keys.map((item)=>{
-                        return( 
-                            <CSSTransition classNames='orders__list-cont' key={item} timeout={{enter: 800, exit: 800}}>
-                                <li key={item} className={`orders__list-item ${!isAvailable(item) ? 'orders__list-outofstock' : ''}`}>
+                <div className='orders__container'>
+                    <h2 className='orders__tittle'>
+                        Your Order 
+                    </h2>
+                    {!keys.length && <h3 className='orders__list-noitem'>Please make your order</h3>}
+                    <TransitionGroup component='ul' className='orders__list-cont'>
+                        {keys.map((item)=>{
+                            return( 
+                                <CSSTransition classNames='orders__list-cont' key={item} timeout={{enter: 800, exit: 800}}>
+                                    <li key={item} className={`orders__list-item ${!isAvailable(item) ? 'orders__list-outofstock' : ''}`}>
 
-                                {!isAvailable(item) &&
-                                    <h4 className='orders__outofstock-warning'>ouof stock temporary</h4> }
-                                    <div className='orders__title-cont'>
-                                        <div className='orders__list-info'>
-                                            <span className={numberClassMaker(item)}>{order[item]['amount']}</span>
-                                            <span className={transitionClassMaker(item)}>{isScrolOrdrNumUp ? order[item]['amount']-1 : order[item]['amount']+1}</span>
-                                        <span> x {burgers[item].name} &nbsp;&nbsp;&nbsp;&#163;{parseFloat(burgers[item].price).toFixed(2)}</span></div>
-                                        <button onClick={()=>{this.props.removeOrder(item); this.setMoveUpCss()}} className='orders__list-button   select-button'>X<span>X</span></button> 
-                                    </div>
-                                
-                                </li>
-                            </CSSTransition>
-                        )
-                    })}
-                </TransitionGroup>
-                <div className='orders__total-cont'>
-                    {total > 0 && <Total total={total}/>}
+                                    {!isAvailable(item) &&
+                                        <h4 className='orders__outofstock-warning'>ouof stock temporary</h4> }
+                                        <div className='orders__title-cont'>
+                                            <div className='orders__list-info'>
+                                                <span className={numberClassMaker(item)}>{order[item]['amount']}</span>
+                                                <span className={transitionClassMaker(item)}>{isScrolOrdrNumUp ? order[item]['amount']-1 : order[item]['amount']+1}</span>
+                                            <span> x {burgers[item].name} &nbsp;&nbsp;&nbsp;&#163;{parseFloat(burgers[item].price).toFixed(2)}</span></div>
+                                            <button onClick={()=>{this.props.removeOrder(item); this.setMoveUpCss()}} className='orders__list-button   select-button'>X<span>X</span></button> 
+                                        </div>
+                                    </li>
+                                </CSSTransition>
+                            )
+                        })}
+                    </TransitionGroup>
+                    <div className='orders__total-cont'>
+                        {total > 0 && <Total total={total}/>}
+                    </div>
                 </div>
+                
             </div>
             
         )
