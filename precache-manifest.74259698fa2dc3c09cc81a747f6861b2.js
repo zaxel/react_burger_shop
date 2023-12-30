aab81a06920f9db3142492f13706b794a58f0322
zaxel <sae11.z.alex@gmail.com>
2023-12-30 14:31:29 +0200

    Updates

---
+++ precache-manifest.74259698fa2dc3c09cc81a747f6861b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0558f513e048b03eb6f821cb22707f5",
+    "url": "/react_boorger_shop/index.html"
+  },
+  {
+    "revision": "d3c34fa1521b0fa7d66b",
+    "url": "/react_boorger_shop/static/css/main.d5738015.chunk.css"
+  },
+  {
+    "revision": "f52c963e522056f194d6",
+    "url": "/react_boorger_shop/static/js/2.be896d38.chunk.js"
+  },
+  {
+    "revision": "d3c34fa1521b0fa7d66b",
+    "url": "/react_boorger_shop/static/js/main.af185ecf.chunk.js"
+  },
+  {
+    "revision": "fa55dd6643fb5d298398",
+    "url": "/react_boorger_shop/static/js/runtime-main.04cdf085.js"
+  },
+  {
+    "revision": "8be34c52443f9cdc982c7c808ae5c197",
+    "url": "/react_boorger_shop/static/media/main.8be34c52.jpg"
+  }
+]);
